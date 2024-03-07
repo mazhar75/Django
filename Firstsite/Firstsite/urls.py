9e1b487ede696cb5fdd2ac262597378ea9698359
mazhar75 <mazharulnihadmd@gmail.com>
2024-03-07 23:16:33 +0600

    Completed

--- Firstsite/Firstsite/urls.py
+++ Firstsite/Firstsite/urls.py
@@ -21,5 +21,5 @@ from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.index, name='index'),
-    path('about',views.about, name='about')
+    path('Conversion',views.Conversion, name='Conversion')
 ]
--- Firstsite/Firstsite/views.py
+++ Firstsite/Firstsite/views.py
@@ -1,8 +1,23 @@
 # This was created by Nihad
 from django.http import HttpResponse
+from django.shortcuts import render
 
-def index(requset):
-    return HttpResponse('Hello, This is Mazharul')
 
-def about(request):
-    return HttpResponse('About Him!')
+def index(request):
+    return render(request,'index.html')
+
+def Conversion(request):
+    text=request.GET.get('textfield','default')
+    Check = request.GET.get('Doit','Off')
+    print(Check)
+    analyzed=''
+    if Check=='on':
+        for char in text:
+            if 'a'<=char<='z':
+                analyzed = analyzed + chr(ord(char)-97+65)
+            else: analyzed = analyzed + char    
+        dict={'purpose':'Without lowercase','analyzed':analyzed}
+        return render(request,'result.html',dict)
+    else :
+        return HttpResponse('Please on the check button')
+    
